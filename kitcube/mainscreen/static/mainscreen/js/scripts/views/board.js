e736909dbc81e6e7f4f0dbad29d8cf557275c610
Alex <aw3s0me@rocketmail.com>
2014-03-25 01:05:11 +0400

    fucked with model

--- kitcube/mainscreen/static/mainscreen/js/scripts/views/board.js
+++ kitcube/mainscreen/static/mainscreen/js/scripts/views/board.js
@@ -1,4 +1,4 @@
-define(['jquery', 'underscore', 'backbone', 'text!templates/board.html'], function($, _, Backbone, boardViewTemplate) {
+define(['jquery', 'underscore', 'backbone', 'text!templates/board.html', 'models/sensorModel', 'text!templates/sensor.html'], function($, _, Backbone, boardTemplate, Sensor, sensorTemplate) {
 	if (!String.prototype.format) {
 		String.prototype.format = function() {
 			var args = arguments;
@@ -36,12 +36,14 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/board.html'], functi
 
 			//var textToParse = options.aceText;
 			var data = {};
-			var compiledTemplate = _.template(boardViewTemplate, data);
+			var compiledTemplate = _.template(boardTemplate, data);
 			console.log(this.container);
 			this.container.append(compiledTemplate);
 			$('.canvas').attr("id", "tab1");
 			this.grid = new kitGrid("#tab1");
-
+			console.log(this.grid.getScale());
+			this.addSensor(4, 2, this.grid.getScale());
+			//console.log(this.grid);
 
 			var marginTop = ($(window).height() - parseInt($('#banner').css('height')) - parseInt($('#footer').css('height')) - this.viewSizeDetector.boardSizeMax.height) / 2;
 			$('.canvas').css('margin-top', marginTop + 'px');
@@ -57,7 +59,47 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/board.html'], functi
 			$('.canvas').data('gridSizeX', this.viewSizeDetector.gridSize.width);
 			$('.canvas').data('gridSizeY', this.viewSizeDetector.gridSize.height);
 			$('.canvas').data('scale', this.viewSizeDetector.scale);
+		},
+		addSensor: function(px, py, scale) {
+			
+			var newSensor = new Sensor({
+				name: "bright", 
+				comment: "doesnt work",
+				unit: "km/h",
+				value: "85",
+				max: "100",
+				min: "15",
+				alarm: "4"});
+			var compiledTemplate = _.template(sensorTemplate, { sensor: newSensor.toJSON() });
+			var jqDivTemplate = $(compiledTemplate);
+			var sensMain = jqDivTemplate.find(".sensMain")[0];
+                sensMain.style.position = 'absolute';
+                sensMain.style.fontSize = 13*scale + 'px';
+                sensMain.style.left = 5*scale + 'px';
+            var sensVal = jqDivTemplate.find(".sensVal")[0]; 
+                //sensVal.id = this.id;
+                sensVal.style.position = 'absolute';
+                sensVal.style.fontSize = 50*scale + 'px';
+                sensVal.style.right = 6*scale + 'px';
+                sensVal.style.bottom = 0*scale + 'px';
+            var sensUnit = jqDivTemplate.find(".sensUnit")[0];
+                sensUnit.style.position = 'absolute';
+                sensUnit.style.fontSize = 12*scale + 'px';
+                sensUnit.style.right = 5*scale + 'px';
+                sensUnit.style.top = 20*scale + 'px';
+                sensUnit.innerHTML = this.unit;
+            var sensClose = jqDivTemplate.find('.sensClose')[0];
+                sensClose.style.position = 'absolute';
+                sensClose.style.fontSize = 12*scale + 'px';
+                sensClose.style.right = 5*scale + 'px';
+                sensClose.style.top = 4*scale + 'px';
+
+			this.grid.addUnit(4, 2, px, py, scale, compiledTemplate);
+
+			console.log($(compiledTemplate));
 		}
+
+
 	});
 
 	// 'jquery', 'underscore', 'backbone' will not be accessible in the global scope
