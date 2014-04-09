ad067f38a116b35b965972d65ee70c61fc24a55f
Nicholas Tan Jerome <nicholas.jerome@kit.edu>
2014-04-09 17:20:39 +0200

    added django-restful app as example and updated dependecies on requirement.txt
    Signed-off-by: Nicholas Tan Jerome <nicholas.jerome@kit.edu>

---
+++ kitcube/snippets/views.py
@@ -0,0 +1,13 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
