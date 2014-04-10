24d2e871e76ec2af65b3d1bfaf88f75cdc91b99d
Nicholas Tan Jerome <nicholas.jerome@kit.edu>
2014-04-10 14:08:57 +0200

    reroute the root index back to mainscreen
    - next to clean up mainpage and add django oauth2
    
    Signed-off-by: Nicholas Tan Jerome <nicholas.jerome@kit.edu>

--- kitcube/kitcube/urls.py
+++ kitcube/kitcube/urls.py
@@ -9,8 +9,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'kitcube.views.home', name='home'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include('snippets.urls')),
-    # url(r'^$', include('mainscreen.urls')),
+    url(r'^snippets/', include('snippets.urls')),
+    url(r'^$', include('mainscreen.urls')),
     url(r'^layout/', include('layout.urls')),
     url(r'^admin/', include(admin.site.urls))
 )
--- kitcube/snippets/urls.py
+++ kitcube/snippets/urls.py
@@ -4,8 +4,8 @@ from snippets import views
 
 
 urlpatterns = patterns('',
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^$', views.SnippetList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 )
