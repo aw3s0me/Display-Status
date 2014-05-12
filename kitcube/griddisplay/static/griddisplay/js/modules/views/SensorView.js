24b6348c91c8deb4a24d370225e68a92ba589b54
Chuan Miao <chuan.miao@kit.edu>
2014-05-12 19:09:38 +0200

    new view: SensorView

---
+++ kitcube/griddisplay/static/griddisplay/js/modules/views/SensorView.js
@@ -0,0 +1,14 @@
+define([
+    'jquery', 
+    'underscore', 
+    'Backbone', 
+    'text!templates/canvas/layout.html', 
+    'common',
+], function($, _, Backbone, Template) {
+
+    var SensorView =  Backbone.View.extend({
+
+    });
+
+    return SensorView;
+});
