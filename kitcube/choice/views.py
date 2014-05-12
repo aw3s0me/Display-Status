1b2f0f44173c92ffc0ce44e65b20ebb1992f3eba
Aleksander Korovin <aw3s0me@rocketmail.com>
2014-05-12 18:49:10 +0200

    Started to write view

--- kitcube/choice/views.py
+++ kitcube/choice/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.template import Context, Template
+from django.http import HttpResponse
 
-# Create your views here.
+def render_choice(request):
+	t = Template("<html></html>") 
+	html = t.render(Context({'field1': 'val1'}))
+	return HttpResponse(html)
