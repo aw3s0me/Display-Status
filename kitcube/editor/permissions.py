9ae13a657332ce35a71763f5a565497b9ffe595f
Aleksander Korovin <aw3s0me@rocketmail.com>
2014-05-19 14:19:04 +0200

    Created permissions

--- kitcube/editor/permissions.py
+++ kitcube/editor/permissions.py
@@ -6,3 +6,33 @@ class IsOwnerOrReadOnly(BasePermission):
             return True
         
         return obj.owner == request.user
+
+#need to rewrite code according to mixins
+#class ReadOnlyMixin()
+#class WriteOnlyMixin()
+#class ReadWriteMixin()
+
+class ReadOnlyPermission(BasePermission):
+	def has_object_permission(self, request, view, obj):
+		if request.method == 'GET':
+			return True
+		return obj.owner == request.user
+
+class WriteOnlyPermission(BasePermission):
+	def has_object_permission(self, request, view, obj):
+		if request.method == 'POST' and request.user and request.is_authenticated():
+			return True
+		return obj.owner == request.user
+
+class ReadWritePermission(BasePermission):
+	def has_object_permission(self, request, view, obj):
+		if request.method == 'POST' or request.method == 'GET':
+			return True
+		return obj.owner == request.user
+
+class AdminOnlyPermission(BasePermission):
+	def has_object_permission(self, request, view, obj):
+		if request.user.is_superuser:
+			return True
+		return obj.owner == request.user
+
